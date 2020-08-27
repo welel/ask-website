deca2d4a51c0f0a58ad769c75cedd5c18ced70e8
Pavel Loginov <pavel.loginov.dev@gmail.com>
2020-08-27 22:18:41 +0300

    new changes, now work fine

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-#sudo cp -rf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
-#sudo service nginx reload
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo cp -rf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf                  
+sudo rm -rf /etc/nginx/sites-enabled/default                                    
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf      
 sudo /etc/init.d/nginx restart
