6a15303817b5ff24e1c652bf5c28e0ad59bbfe64
Pavel Loginov <pavel.loginov.dev@gmail.com>
2020-08-27 22:13:26 +0300

    new changes

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo cp -rf /home/pavel/web/etc/nginx.conf /etc/nginx/nginx.conf
-sudo service nginx reload
-
-# sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-# sudo /etc/init.d/nginx restart
+#sudo cp -rf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
+#sudo service nginx reload
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
