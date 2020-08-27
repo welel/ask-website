31eafdde23c946532ccfbdbafc7f9d03ad61116e
Pavel Loginov <pavel.loginov.dev@gmail.com>
2020-08-27 22:04:10 +0300

    Init repo

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo cp -rf /home/pavel/web/etc/nginx.conf /etc/nginx/nginx.conf
+sudo service nginx reload
+
+# sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# sudo /etc/init.d/nginx restart
