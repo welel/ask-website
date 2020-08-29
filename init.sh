18bd29531c1e045ab7f8e47bdf0d18f6b242ca58
Pavel Loginov <pavel.loginov.dev@gmail.com>
2020-08-29 17:42:00 +0300

    nginx.conf copys to "sites-enabled" now

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-sudo cp -rf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf                  
-sudo rm -rf /etc/nginx/sites-enabled/default                                    
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf      
 sudo /etc/init.d/nginx restart
