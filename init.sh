d00ab811dc21a54582ab2122876f8bb2e50487ca
Pavel Loginov <pavel.loginov.dev@gmail.com>
2020-08-31 16:39:32 +0300

    +New django project "ask"; +requirements for project; changed booting scripts, placed in /bin/*;

--- init.sh
+++
@@ -1,6 +0,0 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default      
-#sudo ln -sf /home/pavel/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-
-sudo service nginx restart
-gunicorn -c etc/gunicorn.conf.py hello:application
-
