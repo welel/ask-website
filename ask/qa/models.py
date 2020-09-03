2c452f96dd68c9bf056e0cdb9ebe53facb33becd
Pavel Loginov <pavel.loginov.dev@gmail.com>
2020-09-03 12:03:49 +0300

    Fix issues with models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -9,24 +9,23 @@ class User(User):
 class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
-
     def popular(self):
         return self.order_by('-rating')
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User,
                                 on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
     objects = QuestionManager()
 
 class Answer(models.Model):
-    text = model.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question,
                                 on_delete=models.CASCADE)
-    author = model.ForeignKey(User,
+    author = models.ForeignKey(User,
                                 on_delete=models.CASCADE)
